df56a9f4dd84792464e06ed93a4e4aaf5d57f899
Rich Kilmer <rich@infoether.com>
2010-01-19 23:06:00 -0500

    template looks in the gem for the template dir now

--- lib/hotcocoa/template.rb
+++ lib/hotcocoa/template.rb
@@ -3,11 +3,15 @@ require 'rbconfig'
 
 module HotCocoa
   class Template
+    
+    def self.source_directory
+      File.expand_path(File.join(File.dirname(__FILE__), "..", ".."))
+    end
+    
     def self.copy_to(directory, app_name)
       FileUtils.mkdir_p(directory)
-      dir = Config::CONFIG['datadir']
-      Dir.glob(File.join(dir, "hotcocoa_template", "**/*")).each do |file|
-        short_name = file[(dir.length+19)..-1]
+      Dir.glob(File.join(source_directory, "template", "**/*")).each do |file|
+        short_name = file[(source_directory.length+10)..-1]
         if File.directory?(file)
           FileUtils.mkdir_p File.join(directory, short_name)
         else
