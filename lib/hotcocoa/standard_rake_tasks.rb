6c1c3c7637b91e319b79e53ab0be9787dd118d0f
dj2 <dj2@everburning.com>
2010-01-21 11:39:15 -0500

    add descriptions to rake tasks so they show up with macrake -T

--- lib/hotcocoa/standard_rake_tasks.rb
+++ lib/hotcocoa/standard_rake_tasks.rb
@@ -1,17 +1,21 @@
 AppConfig = HotCocoa::ApplicationBuilder::Configuration.new("config/build.yml")
 
+desc "Build a deployable version of the application"
 task :deploy => [:clean] do
   HotCocoa::ApplicationBuilder.build(AppConfig, :deploy => true)
 end
 
+desc "Build the application"
 task :build do
   HotCocoa::ApplicationBuilder.build(AppConfig)
 end
 
+desc "Build and execute the application"
 task :run => [:build] do
   `"./#{AppConfig.name}.app/Contents/MacOS/#{AppConfig.name.gsub(/ /, '')}"`
 end
 
+desc "Cleanup build files"
 task :clean do
   `/bin/rm -rf "#{AppConfig.name}.app"`
 end
