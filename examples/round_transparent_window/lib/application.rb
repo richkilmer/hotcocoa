9417cb6d158191f39623bbd44e35194e89ba97c3
dan sinclair <dj2@everburning.com>
2010-01-25 03:52:02 +0800

    fix round_transparent_window example

--- examples/round_transparent_window/lib/application.rb
+++ examples/round_transparent_window/lib/application.rb
@@ -1,8 +1,8 @@
 require 'rubygems'
 require 'hotcocoa'
+
 SOURCE_DIR = File.expand_path(File.dirname(__FILE__))
 require SOURCE_DIR + '/nib_controller'
-require SOURCE_DIR + '/controller'
 require SOURCE_DIR + '/custom_view'
 require SOURCE_DIR + '/custom_window'
 
