550798436be4f9d0e25f34a54a994635fa52ffae
Jamis Buck <jamis@37signals.com>
2010-12-05 11:03:37 -0700

    mappings for segment style constants

--- lib/hotcocoa/mappings/segmented_control.rb
+++ lib/hotcocoa/mappings/segmented_control.rb
@@ -58,6 +58,14 @@ end
 
 HotCocoa::Mappings.map :segmented_control => :NSSegmentedControl do
   
+  constant :segment_style, {
+    :rounded          => NSSegmentStyleRounded,
+    :textured_rounded => NSSegmentStyleTexturedRounded,
+    :round_rect       => NSSegmentStyleRoundRect,
+    :capsule          => NSSegmentStyleCapsule,
+    :small_square     => NSSegmentStyleSmallSquare
+  }
+
   defaults :layout => {}, :frame => [0,0,0,0]
   
   def init_with_options(segmented_control, options)
