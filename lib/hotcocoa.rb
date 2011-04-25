012fca9121e577fb8c3b875c33b15a891ad25a9e
Mark Rada <mrada@marketcircle.com>
2011-04-25 02:36:23 -0400

    Add a docstring for the HotCocoa module

--- lib/hotcocoa.rb
+++ lib/hotcocoa.rb
@@ -3,6 +3,9 @@ require   'set'
 
 STDOUT.reopen(IO.for_fd(NSFileHandle.fileHandleWithStandardError.fileDescriptor.to_i, 'w'))
 
+##
+# HotCocoa is a Cocoa mapping library for MacRuby. It simplifies the use
+# of complex Cocoa classes using DSL techniques.
 module HotCocoa
   Views = {}
 end
