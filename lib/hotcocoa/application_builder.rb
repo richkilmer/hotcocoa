cdfd579dacffe27bd2a5922522fb6fe47ef149ab
Watson <watson1978@gmail.com>
2011-04-30 03:44:13 +0900

    fix a bug that application does not launch.

--- lib/hotcocoa/application_builder.rb
+++ lib/hotcocoa/application_builder.rb
@@ -265,8 +265,8 @@ framework 'Cocoa'
 # Loading all the Ruby project files.
 main = File.basename(__FILE__, File.extname(__FILE__))
 dir_path = NSBundle.mainBundle.resourcePath.fileSystemRepresentation
-Dir.glob(File.join(dir_path, '*.{rb,rbo}')).map { |x| File.basename(x, File.extname(x)) }.uniq.each do |path|
-  if path != main
+Dir.glob(File.join(dir_path, '**/*.{rb,rbo}')).uniq.each do |path|
+  if File.basename(path) != main
     require(path)
   end
 end
