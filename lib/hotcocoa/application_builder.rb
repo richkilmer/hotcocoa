fa225be3b13bf9b68826a4ac3fc6b4aa209007c1
Watson <watson1978@gmail.com>
2011-04-30 05:22:52 +0900

    load a unique file with *.{rb, rbo}

--- lib/hotcocoa/application_builder.rb
+++ lib/hotcocoa/application_builder.rb
@@ -265,9 +265,10 @@ framework 'Cocoa'
 # Loading all the Ruby project files.
 main = File.basename(__FILE__, File.extname(__FILE__))
 dir_path = NSBundle.mainBundle.resourcePath.fileSystemRepresentation
-Dir.glob(File.join(dir_path, '**/*.{rb,rbo}')).uniq.each do |path|
-  if File.basename(path) != main
-    require(path)
+dir_path += "/lib/"
+Dir.glob(File.join(dir_path, '*.{rb,rbo}')).map { |x| File.basename(x, File.extname(x)) }.uniq.each do |path|
+  if path != main
+    require File.join(dir_path, path)
   end
 end
 
