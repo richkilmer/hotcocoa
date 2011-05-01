0867e04390fb6a1478c0361848b95b06247d1c66
Mark Rada <mrada@marketcircle.com>
2011-04-30 21:20:12 -0400

    Document MappingMethods#defaults

--- lib/hotcocoa/mapping_methods.rb
+++ lib/hotcocoa/mapping_methods.rb
@@ -1,6 +1,23 @@
 module HotCocoa
   module MappingMethods
-    def defaults(defaults=nil)
+
+    ##
+    # You can provide a hash of default options in the definition of
+    # your mapping. This is very useful for many Cocoa classes, because
+    # there are so many options to set at initialization.
+    #
+    # The defaults that are set with this method are used in the
+    # constructor to set a value for any keys that are not passed to the
+    # constructor when it is called.
+    #
+    # @overload defaults
+    #   Get the hash of defaults
+    #   @return [Hash,nil] The hash of
+    # @overload defaults key1: value1, key2: value2, ...
+    #   Set the hash of defaults
+    #   @param [Hash]
+    #   @return [Hash]
+    def defaults defaults = nil
       if defaults
         @defaults = defaults
       else
