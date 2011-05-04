2de9e4d8ebd282b7194738ffb1698007edeedb79
Mark Rada <mrada@marketcircle.com>
2011-05-04 00:27:22 -0400

    Add a convenience for running the run loop during tests

--- test/helper.rb
+++ test/helper.rb
@@ -6,5 +6,13 @@ require 'minitest/pride'
 $LOAD_PATH.unshift File.join( File.dirname(__FILE__), '..', 'lib' )
 require 'hotcocoa'
 
+class MiniTest::Unit::TestCase
+
+  def run_run_loop time = 1.0
+    NSRunLoop.currentRunLoop.runUntilDate( Time.now + time )
+  end
+
+end
+
 class SampleClass
 end
