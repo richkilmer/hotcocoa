0786f40ee4d83bc98e67e4b1b27cbf52b32e1270
Mark Rada <mrada@marketcircle.com>
2011-05-04 00:49:27 -0400

    Add some tests for bonjour_service

---
+++ test/mappings/test_bonjour_service.rb
@@ -0,0 +1,62 @@
+##
+# @todo In this test class, there is a base set of tests
+#       that each mapping should have
+class TestBonjourService < MiniTest::Unit::TestCase
+  include HotCocoa
+
+  def test_defaults_kick_in
+    b = bonjour_service type:'_http._tcp.', name:'Defaults Test'
+
+    assert_equal '', b.domain
+    assert_equal -1, b.port
+  end
+
+  def test_defaults_can_be_overridden
+    b = bonjour_service type:'_http._tcp.', name:'Defaults Test',
+                        domain: 'local.', port: 9090
+
+    assert_equal 'local.', b.domain
+    assert_equal 9090,     b.port
+  end
+
+  def test_delegation_publishing_success
+    b = bonjour_service type:'_http._tcp.', name:'HotCocoa Test', port: 9091
+    will_publish = did_publish = did_stop = false
+
+    b.will_publish { will_publish = true }
+    b.did_publish { did_publish = true }
+    b.did_stop { did_stop = true }
+
+    b.publish
+    assert will_publish
+
+    run_run_loop
+    assert did_publish
+
+    b.stop
+    assert did_stop
+  end
+
+  def test_delegation_publishing_failure
+    b = bonjour_service type:'fake', name:'HotCocoa Test', port: 9091
+
+    did_not_publish = false
+
+    b.did_not_publish { |error_dict| did_not_publish = error_dict }
+
+    b.publish
+    run_run_loop
+
+    assert_equal NSNetServicesBadArgumentError,
+      did_not_publish["NSNetServicesErrorCode"]
+  end
+
+  def test_delegating_resolving
+    skip 'TODO'
+  end
+
+  def test_delegation_TXT_record
+    skip 'TODO'
+  end
+
+end
