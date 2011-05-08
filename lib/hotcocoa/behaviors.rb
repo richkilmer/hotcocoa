bc56d0fb7829515ddfdd2e62dc608fd2cfe797d5
Mark Rada <mrada@marketcircle.com>
2011-05-08 02:33:26 -0400

    Document Behaviors

--- lib/hotcocoa/behaviors.rb
+++ lib/hotcocoa/behaviors.rb
@@ -1,7 +1,16 @@
 module HotCocoa
+
   module Behaviors
-    def Behaviors.included(klass)
+
+    ##
+    # Implements the callback Module#included to make sure that classes
+    # that mix in the {Behaviors} module and are themselves mixed in
+    # later will also have custom methods, delegates, etc. properly
+    # set up.
+    def self.included klass
       Mappings::Mapper.map_class(klass)
-    end 
+    end
+
   end
+
 end
