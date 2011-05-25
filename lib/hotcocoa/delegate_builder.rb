9db204112321837e3fc2a1e2cf2a07fda7251a0d
Mark Rada <mrada@marketcircle.com>
2011-05-25 00:43:53 -0400

    Misc. minor documentation for DelegateBuilder

--- lib/hotcocoa/delegate_builder.rb
+++ lib/hotcocoa/delegate_builder.rb
@@ -1,10 +1,24 @@
+##
+# Builds a delegate for a control at runtime by creating a generic object
+# and adding singleton methods for each given delegate method; it then
+# tells the control to delegate to that created object.
 class HotCocoa::DelegateBuilder
 
+  # @return The object that needs a delegate
   attr_reader :control
+
+  # @return [Array<>] Delegate methods which are assumed to be implemented
+  #    and therefore __MUST__ be given at least a stub
   attr_reader :required_methods
+
+  # @return [Fixnum] Number of delegate methods that have been added
   attr_reader :method_count
+
+  # @return [Object] The delegate object
   attr_reader :delegate
 
+  # @param control the object which needs a delegate
+  # @param [Array<String>] required_methods
   def initialize control, required_methods
     @control = control
     @required_methods = required_methods
@@ -12,6 +26,8 @@ class HotCocoa::DelegateBuilder
     @delegate = Object.new
   end
 
+  ##
+  # Add a delegated method for {#control} to {#delegate}
   def add_delegated_method block, selector_name, *parameters
     clear_delegate if required_methods.empty?
 
@@ -34,6 +50,8 @@ class HotCocoa::DelegateBuilder
     delegate.instance_variable_set(block_instance_variable, block)
   end
 
+  ##
+  # @todo GET RID OF EVAL
   def create_delegate_method selector_name, parameters
     required_methods.delete(selector_name)
     eval %{
@@ -43,14 +61,23 @@ class HotCocoa::DelegateBuilder
     }
   end
 
+  ##
+  # Reset the delegate for {#control} to `nil`.
   def clear_delegate
     control.setDelegate(nil) if control.delegate
   end
 
+  ##
+  # Set the delegate for {#control} to {#delegate}
   def set_delegate
     control.setDelegate(delegate)
   end
 
+  ##
+  # Returns an instance variable name to be used for the delegate method
+  # currently being built.
+  #
+  # @return [String]
   def block_instance_variable
     "@block#{method_count}"
   end
